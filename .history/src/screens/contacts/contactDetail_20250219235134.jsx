d41bac12b63196b8cc55b642dbbc41f77c91a251
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-02-19 23:52:28 +0300

    Add contactDetail screen

---
+++ .history/src/screens/contacts/contactDetail_20250219235134.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const contactDetail = () => {
+  return (
+    <View>
+      <Text>contactDetail</Text>
+    </View>
+  );
+};
+
+export default contactDetail;
+
+const styles = StyleSheet.create({});
