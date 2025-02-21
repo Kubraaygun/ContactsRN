8562ec46a9428ed9553f36c6cbef9cceac6f38f3
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-02-21 20:57:40 +0300

    Created calling

--- .history/src/styles/defaultScreenStyle_20250121173301.js
+++
@@ -1,9 +0,0 @@
-import {StyleSheet} from 'react-native';
-import {Colors} from '../theme/colors';
-
-const defaultScreenStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.WHITE,
-  },
-});
